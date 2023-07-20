8ba9678294159ae1e7a9b52b0d25096aa51713e9
Sean Behan <codebam@riseup.net>
2023-07-20 17:03:20 -0400

    view visitors on an api

--- src/worker.ts
+++ src/worker.ts
@@ -1,32 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	// If you set another name in wrangler.toml as the value for 'binding',
+	// replace "DB" with the variable name you defined.
+	DB: D1Database;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env) {
+		const { pathname } = new URL(request.url);
+
+		if (pathname === '/visitors') {
+			// If you did not use `DB` as your binding name, change it here
+			const { results } = await env.DB.prepare('SELECT * FROM visitors').all();
+			return Response.json(results);
+		}
+		if (pathname === '/visitors/add') {
+			const { results } = await env.DB.prepare('INSERT INTO visitors VALUES (?, ?, ?, ?)')
+				.bind(request.cf?.country, request.cf?.city, request.cf?.latitude, request.cf?.longitude)
+				.all();
+		}
+		return new Response('Call /visitors to see everyone who visit. Call /visitors/add to add yourself as a visitor.');
 	},
 };
