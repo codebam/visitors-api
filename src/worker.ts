4df37ad0dba9254b02caf35f01dbf9981c0718c3
Sean Behan <codebam@riseup.net>
2023-07-20 23:50:36 -0400

    remove unneccessary method

--- src/worker.ts
+++ src/worker.ts
@@ -4,15 +4,13 @@ export interface Env {
 	DB: D1Database;
 }
 
-const console_error = (e: Error) => console.error(e);
-
 const response_all_visitors = async (ctx: RouteContext): Promise<Response> => {
 	const { results } = await ctx.env.DB.prepare('SELECT country, city, latitude, longitude FROM visitors').all();
 	return Response.json(results);
 };
 
 const visitors_count = async (ctx: RouteContext): Promise<number> => {
-	const { results } = await ctx.env.DB.prepare('SELECT COUNT(*) as count from visitors').all().catch(console_error);
+	const { results } = await ctx.env.DB.prepare('SELECT COUNT(*) as count from visitors').all().catch(console.error);
 	return results[0]?.count;
 };
 
@@ -22,11 +20,11 @@ const router = new WorkerRouter()
 		await ctx.env.DB.prepare('INSERT INTO visitors VALUES (?, ?, ?, ?, ?)')
 			.bind(req.cf?.country, req.cf?.city, req.cf?.latitude, req.cf?.longitude, req.headers.get('x-real-ip'))
 			.all()
-			.catch(console_error);
+			.catch(console.error);
 		return response_all_visitors(ctx);
 	})
 	.get('/visitors/remove', async (req, ctx) => {
-		await ctx.env.DB.prepare('DELETE FROM visitors WHERE ip = ?').bind(req.headers.get('x-real-ip')).all().catch(console_error);
+		await ctx.env.DB.prepare('DELETE FROM visitors WHERE ip = ?').bind(req.headers.get('x-real-ip')).all().catch(console.error);
 		return response_all_visitors(ctx);
 	})
 	.any('*', async (_req, ctx) =>
